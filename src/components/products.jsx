f6d11106a84f1fbe66be51eb3bcade14efa7006c
samusun <samuelsinkorg@gmail.com>
2021-10-24 20:39:59 +0200

    added upload func, where firabase storage and firestore is kinda connected. Also update in marketplace

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,41 +1,64 @@
 import React, { useEffect, useState } from 'react';
-import { db } from '../firebase';
+import { Link } from 'react-router-dom';
+import { db, storage } from '../firebase';
 import { Card, Button } from 'react-bootstrap';
-import ye from '../assets/pictures/ye.png';
 import s from '../scss/components/products.module.scss';
 
 export default function Products() {
+  const storageRef = storage.ref();
   const [data, setData] = useState([]);
-  function run(id) {
-    console.log(id);
-  }
+  const [imgData, setImgData] = useState([]);
+
   useEffect(() => {
-    db.collection('art')
-      .get()
-      .then((snapshot) => {
-        setData(snapshot.docs);
-      });
+    let newArr = [];
+    function getData() {
+      db.collection('art')
+        .get()
+        .then((snapshot) => {
+          setData(snapshot.docs);
+        });
+    }
+    getData();
+
+    for (let i = 0; i < data.length; i++) {
+      console.log('forloop is running');
+      storageRef
+        .child(`uploads/${data[i].data().imgId}`)
+        .getDownloadURL()
+        .then((url) => {
+          newArr.push(url);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    setImgData(newArr);
+    console.log(imgData);
+    let allArr = data;
+    for (let i = 0; i < data.length; i++) {
+      allArr[i].imgUrl = imgData[i];
+    }
+    setData(allArr);
+    console.log(data);
   }, []);
 
   return (
-    <div>
-      <div className={s.container}>
-        {data.map((item) => (
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant='top' src={ye} />
-            <Card.Body>
-              <Card.Title>{item.data().name}</Card.Title>
-              <Card.Text>
-                <p> By: {item.data().artist} </p>
-                <p> Price: {item.data().price} </p>
-              </Card.Text>
-              <Button onClick={() => run(item.id)} variant='dark'>
-                Go somewhere
-              </Button>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
+    <div className={s.container}>
+      {data.map((item) => (
+        <Card key={item.id} style={{ width: '18rem' }}>
+          <Card.Img variant='top' src={item.data().url} />
+          <Card.Body>
+            <Card.Title>{item.data().name}</Card.Title>
+            <Card.Text>
+              <p> By: {item.data().artist} </p>
+              <p> Price: {item.data().price} </p>
+            </Card.Text>
+            <Link to={`marketplace/${item.id}`}>
+              <Button variant='dark'>Go to product</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Products from './../components/products';
+import s from '../scss/pages/marketplace.module.scss';
 
 export default function Marketplace() {
   return (
-    <div>
-      <h1>This is marketplace</h1>
+    <div className={s.container}>
+      <h1>Marketplace</h1>
       <Products />
     </div>
   );
